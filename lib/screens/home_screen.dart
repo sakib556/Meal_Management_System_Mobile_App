776457087440213858deb3210597c3c16ef7928f
Sakib556 <kazisakib556@gmail.com>
2023-10-21 14:18:50 +0600

    landing screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,160 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:meal_management/screens/login_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      drawer: buildDrawer(context),
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        title: const Text(
+          'Meal Management',
+          // style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green[500],
+      ),
+      body: const Center(
+          child: SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text("Home screen"),
+            )),
+      )),
+    );
+  }
+
+  bool isLogoutLoading = false;
+  Widget buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.green[500],
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Hi mr/ms',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  FirebaseAuth.instance.currentUser!.email ?? "",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+          isLogoutLoading
+              ? const CircularProgressIndicator()
+              : ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('Logout'),
+                  onTap: () {
+                    _logout(
+                        context); // Call the logout function with the context
+                  },
+                ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _logout(BuildContext context) async {
+    try {
+      isLogoutLoading = true;
+      setState(() {});
+      await FirebaseAuth.instance.signOut();
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        await FirebaseDatabase.instance
+            .ref()
+            .child('users/${user.uid}/device_token')
+            .remove();
+      }
+      isLogoutLoading = false;
+      setState(() {});
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+      // After logout, navigate to the LoginScreen
+    } catch (e) {
+      isLogoutLoading = false;
+      setState(() {});
+      EasyLoading.showError(e.toString());
+      print('Error during logout: $e');
+    }
+  }
+}
+
+class RealTimeDataWidget extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+
+  const RealTimeDataWidget(
+      {super.key,
+      required this.icon,
+      required this.label,
+      required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+// /      height: 150,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.green[500],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 50,
+            color: Colors.white,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            label,
+            style: const TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            value,
+            style: const TextStyle(color: Colors.white, fontSize: 24),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:meal_management/screens/home_screen.dart';
+import 'package:meal_management/screens/login_screen.dart'; // Import the FirebaseDatabase package
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  Future<DataSnapshot?>? loginCheck;
+
+  @override
+  void initState() {
+    super.initState();
+    loginCheck = checkUserLoginStatus();
+  }
+
+  Future<DataSnapshot?> checkUserLoginStatus() async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        return await getUserDeviceToken(user.uid);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error checking user login status: $e');
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: FutureBuilder<DataSnapshot?>(
+        future: loginCheck,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            );
+          } else if (snapshot.hasData) {
+            if (snapshot.data != null && snapshot.data!.value != null) {
+              return const HomeScreen();
+            } else {
+              return const LoginScreen();
+              //return const LoginScreen();
+            }
+          } else {
+            // Handle the case when an error occurs or snapshot.data is null
+            return const LoginScreen();
+            //return const LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<DataSnapshot?> getUserDeviceToken(String userId) async {
+  DataSnapshot? snapshot;
+  try {
+    // Use the DatabaseEvent and access its snapshot property
+    DatabaseEvent event = await FirebaseDatabase.instance
+        .ref()
+        .child('users/$userId/device_token')
+        .once();
+    snapshot = event.snapshot;
+  } catch (e) {
+    print('Error getting user device token: $e');
+  }
+  return snapshot;
+}
